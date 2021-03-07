1130b287c52625ec8c41de9e45a09ced455366eb
Makai Post <postmakai@gmail.com>
2021-03-07 11:00:13 -0800

    changed test for footer

--- client/src/components/PageFooter.js
+++ client/src/components/PageFooter.js
@@ -2,7 +2,7 @@ const PageFooter = () => {
     return (
         <footer>
             <hr/>
-            <small>Made with ❤️️ by Makai Post, Aaron Parnell, Danny Zosh, & Mike Belliveau</small>
+            <small>Made by Makai Post, Aaron Parnell, Danny Zosh, & Mike Belliveau</small>
             <small>© 2021</small>
         </footer>
     )
