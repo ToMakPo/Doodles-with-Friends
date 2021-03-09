72775526e6581e907dea08724ddbf96a3915c9ca
Danny Maier <dannymaier@gmail.com>
2021-03-08 19:58:28 -0800

    added a template component folder. Added an input form component.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/doodle_db", {
 })
 
 // routes
-app.use(require("./routes/api.js"))
+// app.use(require("./routes/api.js"))
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`)
