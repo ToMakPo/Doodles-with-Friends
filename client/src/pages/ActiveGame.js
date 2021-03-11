c67ca51a3ff624f479b5156972335298db56a557
Mike <bigsexymike@Daniels-MacBook-Pro.local>
2021-03-10 21:06:14 -0800

    Roughed out ActiveGame.js

--- client/src/pages/ActiveGame.js
+++ client/src/pages/ActiveGame.js
@@ -4,6 +4,29 @@ const ActiveGame = () => {
     return (
         <main>
             <h2>Active Game</h2>
+            <div className="container">
+
+                <h3>THE WORD</h3>
+
+                <h3>ROUND X OF 10</h3>
+
+                <h3>TIME REMAINING:</h3>
+
+                </div>
+
+            <div className="container">
+
+                <h3>DRAWING AREA</h3>
+
+            </div>
+
+            <div className="container">
+
+                <h3>CALL CHAT HERE</h3>
+
+            </div>
+
+
             <Canvas width={500} height={500} active={true}/>
         </main>
     )
