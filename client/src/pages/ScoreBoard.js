ae5527c99e4997432d935e7d13b1ae4bb035f735
Danny Maier <dannymaier@gmail.com>
2021-03-10 22:42:58 -0800

    cleaned up the CSS on the scoreboard and the options pages

--- client/src/pages/ScoreBoard.js
+++ client/src/pages/ScoreBoard.js
@@ -1,3 +1,4 @@
+import ChatBox from "../components/ChatBox"
 import '../styles/palette.css'
 import '../styles/ScoreBoard.css'
 
@@ -6,19 +7,39 @@ const ScoreBoard = () => {
     return (
         <div className="container ">
             <main className="container">
-            <div 
-            className="card" 
-            style={{width: "18rem"}}
-            >
-                <h2 className="card-header">
-                    Score Board
-                </h2>
-                <ol className="list-group list-group-flush">
-                    <li className="list-group-item">An item</li>
-                    <li className="list-group-item">A second item</li>
-                    <li className="list-group-item">A third item</li>
-                </ol>
-            </div>
+
+
+
+                <h2>FINISHED</h2>
+                <div class="card-deck">
+                        <div className="card">
+                            <h2 className="card-header">
+                                Score Board
+                            </h2>
+                            <ol className="list-group list-group-flush">
+                                <li className="list-group-item">An item</li>
+                                <li className="list-group-item">A second item</li>
+                                <li className="list-group-item">A third item</li>
+                            </ol>
+                        </div>
+
+
+                    <div class="card">
+                        <div class="card-body">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">PLAY AGAIN</button>
+                        </div>
+                    </div>
+                </div>
+<br></br>
+                <div class="container">
+                <div class="card">
+                        <div class="card-body">
+                    <div class="row align-items-end">
+                        <ChatBox/>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </main>
 
         </div>
