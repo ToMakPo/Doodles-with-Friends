46758134986dc89a3c006d9b6c6761084e744785
Danny Maier <dannymaier@gmail.com>
2021-03-13 09:53:03 -0800

    bootstrappped the options page

--- client/src/pages/WaitingRoom.js
+++ client/src/pages/WaitingRoom.js
@@ -3,39 +3,38 @@ import '../styles/WaitingRoom.css'
 
 const WaitingRoom = () => {
     return (
+        <div className="container ">
         <main>
             <h2>Waiting Room</h2>
 
-            <div className="container">
-            <div className="row">
+            <div className="card-deck">
+
 {/* Column 1 */}
-                <div className="col-sm ">
-                    <div>
-                        <div className="card-body">
-                            <h4 className="card-title">Your Game Code</h4>
-                            <p> 8675309</p>
-                        </div>
-                        <div className="card-body">
-                            <h4 className="card-title">Attending Players</h4>
-                            <ol>
-                                <li>Player 1</li>
-                                <li>Player 2</li>
-                                <li>Player 3</li>
-                                <li>Player 4</li>
-                                <li>Player 5</li>
-                                <li>Player 6</li>
-                            </ol>
-                        </div>
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="card-title">Game Code:</h3>
+                        <h5> 8675309</h5>
+                    </div>
+                    <div className="card-body">
+                        <h4 className="card-title">Attending Players</h4>
+                        <ol>
+                            <li>Player 1</li>
+                            <li>Player 2</li>
+                            <li>Player 3</li>
+                            <li>Player 4</li>
+                            <li>Player 5</li>
+                            <li>Player 6</li>
+                        </ol>
                     </div>
                 </div>
 {/* Column 2 */}
-                <div className="col-sm ">
-                    <div>
-                        <div className="card-body">
-                            <h4 className="card-title">Category:</h4>
-                            <p> Animals</p>
-                        </div>
-                        <div className="card-body">
+                <div className="card ">
+                    <div className="card-header">
+                        <h3 className="card-title">Category:</h3>
+                        <h5> Animals</h5>
+                    </div>
+                    <div className="card-body">
+                        <div>
                             <input 
                             type="text" 
                             className="form-control" 
@@ -44,7 +43,9 @@ const WaitingRoom = () => {
                             aria-describedby="basic-addon2"
                             // style={{ width: "25%"}}
                             />
-                            <br></br>
+                        </div>
+                        <br></br>
+                        <div>
                             <h4 className="card-title">Added Words</h4>
                             <ol>
                                 <li>Word 1</li>
@@ -56,19 +57,22 @@ const WaitingRoom = () => {
                             </ol>
                         </div>
                     </div>
+
                 </div>
 {/* Column 3 */}
-                <div className="col-sm">
+                <div className="card">
                     <div className="card-body">
-                        <button className="btn btn-outline-secondary" type="button">Start Game</button>
+                        <button className="
+                        btn btn-primary 
+                        btn-lg 
+                        btn-block" type="button">Start Game</button>
                     </div>
                 </div>
+            {/* </div> */}
             </div>
-            </div>
-
-
-
         </main>
+    
+        </div>
     )
 }
 
