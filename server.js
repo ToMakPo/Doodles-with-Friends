9b3f96c87bafe553f88ebd2906292955ce779047
Makai Post <postmakai@gmail.com>
2021-03-18 21:45:30 -0700

    fixed

--- server.js
+++ server.js
@@ -42,44 +42,44 @@ io.on('connection', newConnection(socket))
 function newConnection(socket, io) {
     console.log('new connection:', socket.id)
 
-    socket.on('setColor', (gameId, color) => {
-        socket.broadcast.emit(`${gameId}-setColor`, color)
+    socket.on('setColor', (lobbyId, color) => {
+        id.emit(`${lobbyId}-setColor`, color)
     })
-    socket.on('setSize', (gameId, size) => {
-        socket.broadcast.emit(`${gameId}-setSize`, size)
+    socket.on('setSize', (lobbyId, size) => {
+        id.emit(`${lobbyId}-setSize`, size)
     })
-    socket.on('startLine', (gameId, x, y) => {
-        socket.broadcast.emit(`${gameId}-startLine`, x, y)
+    socket.on('startLine', (lobbyId, x, y) => {
+        id.emit(`${lobbyId}-startLine`, x, y)
     })
-    socket.on('drawLine', (gameId, x, y) => {
-        socket.broadcast.emit(`${gameId}-drawLine`, x, y)
+    socket.on('drawLine', (lobbyId, x, y) => {
+        id.emit(`${lobbyId}-drawLine`, x, y)
     })
-    socket.on('endLine', (gameId) => {
-        socket.broadcast.emit(`${gameId}-endLine`)
+    socket.on('endLine', (lobbyId) => {
+        id.emit(`${lobbyId}-endLine`)
     })
-    socket.on('clearDrawing', (gameId) => {
+    socket.on('clearDrawing', (lobbyId) => {
         console.log('sensed clearDrawing');
-        socket.broadcast.emit(`${gameId}-clearDrawing`)
+        id.emit(`${lobbyId}-clearDrawing`)
     })
-    socket.on('usePen', (gameId) => {
-        socket.broadcast.emit(`${gameId}-usePen`)
+    socket.on('usePen', (lobbyId) => {
+        id.emit(`${lobbyId}-usePen`)
     })
-    socket.on('useEraser', (gameId) => {
-        socket.broadcast.emit(`${gameId}-useEraser`)
+    socket.on('useEraser', (lobbyId) => {
+        id.emit(`${lobbyId}-useEraser`)
     })
-    socket.on('logMessage', (gameId, sender, message) => {
-        console.log('game id:', gameId);
-        socket.broadcast.emit(`${gameId}-logMessage`, sender, message)
+    socket.on('logMessage', (lobbyId, sender, message) => {
+        console.log('game id:', lobbyId);
+        id.emit(`${lobbyId}-logMessage`, sender, message)
     })
-    socket.on('logGuess', (gameId, sender, guess) => {
-        socket.broadcast.emit(`${gameId}-logGuess`, sender, guess)
+    socket.on('logGuess', (lobbyId, sender, guess) => {
+        id.emit(`${lobbyId}-logGuess`, sender, guess)
         const answer = 'panda'//TODO: lookup answer for this game
         if (guess.toLowerCase() === answer) {
-            io.emit(`${gameId}-guessIsCorrect`, sender, answer)
+            io.emit(`${lobbyId}-guessIsCorrect`, sender, answer)
             //TODO: trigger next round
         }
     })
-    socket.on('consoleLog', (gameId, message) => {
-        io.emit(`${gameId}-consoleLog`, message)
+    socket.on('consoleLog', (lobbyId, message) => {
+        io.emit(`${lobbyId}-consoleLog`, message)
     })
 }
