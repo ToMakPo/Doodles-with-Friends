b959e0e23772bd71065a265e1d2672e9e487aa8e
Makai Post <postmakai@gmail.com>
2021-03-18 20:02:54 -0700

    fixed merge conflicts

---
+++ client/src/components/LogoutButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useLogout } from '../utils/auth'
+
+
+function LogoutButton() {
+    const logout = useLogout();
+    return <button onClick={logout}>Logout</button>
+}
+
+export default LogoutButton
