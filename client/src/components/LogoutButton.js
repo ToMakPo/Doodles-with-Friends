f644eb977b50e90d1552d36c35da417cd25a7d66
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-18 19:57:46 -0700

    merge conflict

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLogout } from '../utils/auth'
 
+
 function LogoutButton() {
     const logout = useLogout();
     return <button onClick={logout}>Logout</button>
