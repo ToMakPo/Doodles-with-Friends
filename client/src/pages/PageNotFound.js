237fcd99c33c02f8750ac257fd299e5e0e820881
Danny Maier <dannymaier@gmail.com>
2021-03-21 10:58:59 -0700

    removed dead code from the page not found"

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,15 +1,6 @@
 const PageNotFound = () => {
     return (
         <div className="card-body row">
-        <h5 className="card-title col "> Custom Categories:</h5>
-        <div className="row sliderContainer">
-        <p className="col">NO</p>
-        <label className="switch col">
-            <input type="checkbox"/>
-            <span className="slider round"></span>
-        </label>
-        <p className="col">YES</p>
-        </div>
             <main>
                 {/* <div class="card-deck"> */}
                     <div class="card">
