122439d1e6e78fffd45b540d7833d491035ae30a
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-22 09:41:20 -0700

    added functionality to get current lobby from id in url

--- client/src/pages/ArtistView.js
+++ client/src/pages/ArtistView.js
@@ -1,24 +1,37 @@
+import { useEffect, useState } from "react"
 import ChatBox from "../components/ChatBox"
+import API from "../utils/API"
 const { default: Canvas } = require("../components/Canvas")
 
 const ArtistView = () => {
+    const [lobby, setLobby] = useState()
+    useEffect(() => {
+        const lobbyId = window.location.pathname.split('game/')[1]
+        API.getLobby(lobbyId)
+            .then(data => {
+                setLobby(data.data[0])
+
+            })
+            .catch(err => console.error(err))
+    }, [])
+    console.log(lobby)
     return (
-        <div 
-        id="bootstrap-overrides" 
-        className="container sketchBackground">
+        <div
+            id="bootstrap-overrides"
+            className="container sketchBackground">
             <main>
                 <h2 className="banner">
                     <div className="d-flex justify-content-around align-items-center">
-                        
+
                         <div className="d-inline p-2">
-                        THE WORD
+                            THE WORD: { }
                         </div>
                         <div className="d-inline p-2 ">
-                        ROUND 1 OF 5
+                            ROUND 1 OF 5
                         </div>
 
                         <div className="d-inline p-2 ">
-                        TIME REMAINING
+                            TIME REMAINING
                         </div>
                     </div>
                 </h2>
@@ -26,11 +39,11 @@ const ArtistView = () => {
                     <div className="card">
                         <div className="card-body">
                             <div className="">
-                                {/* <Canvas width={500} height={500} active={true}/> */}
+                                <Canvas width={500} height={500} active={true} />
                             </div>
                         </div>
                     </div>
-                    <ChatBox/>
+                    <ChatBox />
                 </div>
 
             </main>
